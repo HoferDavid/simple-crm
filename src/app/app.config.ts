67cca2412e9ff3c8835378f81fc93e70416151ac
David Hofer <chassis.poppets0x@icloud.com>
2024-09-30 17:48:30 +0200

    apiKey Update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,12 +15,12 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideFirebaseApp(() =>
       initializeApp({
-        projectId: 'simple-crm-71ddf',
-        appId: '1:596696624017:web:79f7825fda6847b9f4c830',
-        storageBucket: 'simple-crm-71ddf.appspot.com',
-        apiKey: 'AIzaSyCdV4_DTwm14FLEqLX-bXmkNHnBkGfmsQ8',
-        authDomain: 'simple-crm-71ddf.firebaseapp.com',
-        messagingSenderId: '596696624017',
+        apiKey: process.env['FIREBASE_API_KEY'],
+        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+        projectId: process.env['FIREBASE_PROJECT_ID'],
+        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+        messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+        appId: process.env['FIREBASE_APP_ID'],
       })
     ),
     provideFirestore(() => getFirestore()),
